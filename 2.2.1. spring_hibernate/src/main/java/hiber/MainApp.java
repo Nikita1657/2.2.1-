75711725d7f0ed8a9fcbe268d2dc0569d7ab3a57
Nikita1657 <greuglandq@gmail.com>
2025-03-18 04:57:55 +0300

    1th

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,10 +1,12 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -15,20 +17,40 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User NIK = new User("Никита", "Костин", "nikitakostin@mail.io");
+      User PETR = new User("Петр", "Борт", "BORTMACHANIK@mail.io");
+      User OLEG = new User("Олег", "Монгол", "nizhnii@mail.io");
+      User GLAD = new User("Денис", "Петров", "glad@mail.io");
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+      Car volvo = new Car("Volvo", 1);
+      Car bmw = new Car("BMW", 2);
+      Car suzuki = new Car("Sisuki", 3);
+      Car lada = new Car("Ladaa", 4);
+      Car bentley = new Car("Bentley", 228);
+      Car cherry = new Car("Cherry", 14);
+
+      userService.add(NIK.setCar(volvo).setUser(NIK));
+      userService.add(PETR.setCar(bmw).setUser(PETR));
+      userService.add(OLEG.setCar(suzuki).setUser(OLEG));
+      userService.add(GLAD.setCar(bentley).setUser(GLAD));
+      userService.add(GLAD.setCar(cherry).setUser(GLAD));
+
+      // poisk user with car
+      for (User user : userService.listUsers()) {
+         System.out.println(user + " " + user.getCar());
+      }
+
+      // drop user po seerir and model
+      System.out.println(userService.getUserByCar("BMW", 2));
+
+      // net user
+      try {
+         User notFoundUser = userService.getUserByCar("LUAZ", 42);
+      } catch (NoResultException e) {
+         System.out.println("User not found");
       }
 
       context.close();
+
    }
 }
